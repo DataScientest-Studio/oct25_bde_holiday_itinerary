fd49e6a153f31538129ee348b7ff19b744e49f4f
Max Duessler <max.duessler@posteo.net>
2025-11-15 07:23:02 +0100

    Added function to get nearby points.

--- src/neo4j_api/driver.py
+++ src/neo4j_api/driver.py
@@ -16,10 +16,25 @@ class Neo4jDriver:
         signal(SIGINT, self.handle_exit_signal)
         signal(SIGTERM, self.handle_exit_signal)
 
-    def execute_query(self, query: str, **kwargs: dict[Any, Any]) -> list[Record] | None:
+    def execute_query(self, query: str, **kwargs: Any) -> list[Record] | None:
         with self.driver.session() as session:
             records = session.run(query, kwargs)
             return [record for record in records]
+
+    def get_nearby_points(self, poi_id: str, radius: float) -> list[Record] | None:
+        query = """
+            MATCH (p1:Poi {id: $poi_id})
+            MATCH (p2:Poi)
+            WHERE p1 <> p2
+              AND distance(
+                    point({latitude: p1.x, longitude: p1.y}),
+                    point({latitude: p2.x, longitude: p2.y})
+                  ) <= $radius
+            RETURN p2.id AS id, p2.label AS label, p2.x AS lat, p2.y AS lon
+        """
+        if records := self.execute_query(query, poi_id=poi_id, radius=radius):
+            return [dict(record) for record in records]
+        return None
 
     def close(self) -> None:
         if self.driver:
