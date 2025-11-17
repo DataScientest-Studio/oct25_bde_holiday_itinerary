327038b9cb3e5adf202fb02739275e68575244b0
Max Duessler <max.duessler@posteo.net>
2025-11-17 06:01:47 +0100

    Added interface to get the shortest path in a set of pois.

--- src/neo4j_api/api.py
+++ src/neo4j_api/api.py
@@ -17,3 +17,8 @@ def get_poi(poi_id: str) -> dict[str, Any]:
 @app.get("/nearby")  # type: ignore
 def get_nearby_points(poi_id: str, radius: float) -> dict[str, Any]:
     return driver.get_nearby_points(poi_id, radius)
+
+
+@app.get("/calculate-shortest-path")  # type: ignore
+def calculate_shortest_path(poi_ids: list[str]) -> dict[str, Any]:
+    return driver.calculate_shortest_path(poi_ids)
