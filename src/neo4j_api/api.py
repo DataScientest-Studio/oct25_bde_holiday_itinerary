6b45f06dcda2db0e7e3a132ead92e33da68be82a
Max Duessler <max.duessler@posteo.net>
2025-11-17 05:14:04 +0100

    Added endpoint, to get nearby points.

--- src/neo4j_api/api.py
+++ src/neo4j_api/api.py
@@ -12,3 +12,8 @@ driver = Neo4jDriver()
 @app.get("/poi")  # type: ignore
 def get_poi(poi_id: str) -> dict[str, Any]:
     return driver.get_poi(poi_id)
+
+
+@app.get("/nearby")  # type: ignore
+def get_nearby_points(poi_id: str, radius: float) -> dict[str, Any]:
+    return driver.get_nearby_points(poi_id, radius)
