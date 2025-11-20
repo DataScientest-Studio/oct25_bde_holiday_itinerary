60f75e07be8c7ead18d54f94b46fa8cb0196bf25
Max Duessler <max.duessler@posteo.net>
2025-11-20 10:10:34 +0100

    Fixed wrong function.

--- src/neo4j_api/api.py
+++ src/neo4j_api/api.py
@@ -26,7 +26,7 @@ def shortest_round_tour_visiting_all_nodes(poi_ids: list[str] = Query(...)) -> d
 
 @app.get("/shortest-path-between-all-nodes-with-fixed-start")  # type: ignore
 def shortest_path_between_all_nodes_with_fixed_start(poi_ids: list[str] = Query(...)) -> dict[str, list[str] | float]:
-    return driver.shortest_round_tour_visiting_all_nodes(poi_ids)
+    return driver.shortest_path_between_all_nodes_with_fixed_start(poi_ids)
 
 
 @app.get("/shortest-path-between-all-nodes-with-fixed-end")  # type: ignore
