aad5516f1117988fc59aacbbeb0860b285065024
Max Duessler <max.duessler@posteo.net>
2025-11-21 02:52:59 +0100

    Added code, to initialize the driver once.

--- src/neo4j_api/main.py
+++ src/neo4j_api/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 
+from neo4j_driver.driver import Neo4jDriver
+
 from .routes import dijkstra, distance, poi, tsp
 
 app = FastAPI()
+
+
+@app.on_event("startup")  # type: ignore[misc]
+async def startup_event() -> None:
+    app.state.driver = Neo4jDriver()
+
+
+@app.on_event("shutdown")  # type: ignore[misc]
+async def shutdown_event() -> None:
+    await app.state.driver.close()
+
 
 app.include_router(poi.router, prefix="/poi", tags=["POI"])
 app.include_router(distance.router, prefix="/distance", tags=["Distance"])
