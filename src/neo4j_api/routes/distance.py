caecac113f5fa5b8b1e20a4f08a41d3c57b01949
Max Duessler <max.duessler@posteo.net>
2025-11-21 02:58:28 +0100

    Load same driver in each route.

--- src/neo4j_api/routes/distance.py
+++ src/neo4j_api/routes/distance.py
@@ -5,4 +5,5 @@ router = APIRouter()
 
 @router.get("/")  # type: ignore[misc]
 def get_distance(request: Request, poi1_id: str, poi2_id: str) -> dict[str, float]:
-    return {"distance": request.app.state.driver.calculate_distance_between_two_nodes(poi1_id, poi2_id)}
+    driver = request.app.state.driver
+    return {"distance": driver.calculate_distance_between_two_nodes(poi1_id, poi2_id)}
