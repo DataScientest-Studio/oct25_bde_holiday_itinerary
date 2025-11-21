bfbc7768ad3bf6bf4aa749eeaf6b2fc3bfb10263
Max Duessler <max.duessler@posteo.net>
2025-11-21 02:26:21 +0100

    Made route.

--- src/neo4j_api/routes/distances.py
+++ src/neo4j_api/routes/distances.py
@@ -6,6 +6,6 @@ router = APIRouter()
 driver = Neo4jDriver()
 
 
-@router.get("/distance")  # type: ignore
+@router.get("/")  # type: ignore
 def get_distance(poi1_id: str, poi2_id: str) -> dict[str, float]:
     return {"distance": driver.calculate_distance_between_two_nodes(poi1_id, poi2_id)}
