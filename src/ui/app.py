1b9609ad0ee7b77d3e11ced2c6ffdac0e7165bec
Max Duessler <max.duessler@posteo.net>
2025-12-18 08:48:12 +0100

    Added sessions.

--- src/ui/app.py
+++ src/ui/app.py
@@ -6,8 +6,10 @@ class UI:
         # Use session, to keep data, if an other even occurs.
         # Streamlit clears the app to default back after an
         # event occurs.
-        if "poi" not in st.session_state:
-            st.session_state.poi = ""
+        if "search" not in st.session_state:
+            st.session_state.search = ""
+        if "add-pois" not in st.session_state:
+            st.session_state.add_pois = ""
         if "search_result" not in st.session_state:
             st.session_state.search_result = ""
         st.set_page_config(layout="wide")
@@ -22,32 +24,36 @@ class UI:
         self.cell_two(col_right)
 
     def search_poi(self):
-        poi_query = st.session_state.poi
-        st.session_state.search_result = f"Searching for: {poi_query}"
+        search = st.session_state.search
+        st.session_state.search_result = f"Searching for: {search}"
+
+    def add_pois(self):
+        pass
 
     def cell_one(self, cell) -> None:
         cell.header("Selected POIs")
         listing = cell.container(border=True, height=200)
         for i in range(0, 100):
             listing.write(f"Place{i}")
-        col_1, col_2 = cell.columns([2, 3])
+        col_1, _ = cell.columns([2, 3])
         if col_1.button("Plan Itinerary"):
             col_1.write("IMPLEMENT LOGIC to plan route.")
-        self.search_component(col_2)
-
-    def search_component(self, cell) -> None:
-        col_1, col_2 = cell.columns([2, 1])
-        col_1.text_input("Search", placeholder="Type a point of interest...", key="poi", label_visibility="collapsed")
-        col_2.button("Search", on_click=self.search_poi)
 
     def cell_two(self, cell) -> None:
         cell.header("Filters")
         col_1, col_2 = cell.columns([1, 1])
         col_1.multiselect("Place / City to visit", options=["Paris", "Village"], key="visit_location")
         col_1.multiselect("type of Place / City", options=["City", "Village"], key="location_type")
-        col_1.multiselect("POIs", options=["Louvre", "Cafe", "Eisdiele"], key="location_type")
+        col_1.multiselect("POIs", options=["Louvre", "Cafe", "Eisdiele"], key="add-pois")
+        col_1.button("Add POIs", on_click=self.add_pois)
         col_2.date_input("Start", format="DD/MM/YYYY")
         col_2.date_input("End", format="DD/MM/YYYY")
+        self.search_component(col_2)
+
+    def search_component(self, cell) -> None:
+        col_1, col_2 = cell.columns([2, 1])
+        col_1.text_input("Search", placeholder="Type a point of interest...", key="search")
+        col_2.button("Search", on_click=self.search_poi)
 
     def run(self):
         self.create_rows()
