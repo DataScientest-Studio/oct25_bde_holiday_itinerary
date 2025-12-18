d1793cd109bb6c853c9df571056d370752ad8b91
Max Duessler <max.duessler@posteo.net>
2025-12-18 07:10:51 +0100

    Implemented UI class.

---
+++ src/ui/app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+
+class UI:
+
+    def __init__(self) -> None:
+        self.header()
+
+    def header(self):
+        st.title("Holiday Itinerary")
+
+    def create_rows(self):
+        self.first_row()
+        self.second_row()
+
+    def first_row(self):
+        col_left, col_right = st.columns([1, 3])
+        with col_left:
+            st.title("POIs")
+
+        with col_right:
+            st.title("Filter")
+
+    def second_row(self):
+        col_left, col_right = st.columns([3, 1])
+        with col_left:
+            st.title("Map")
+
+        with col_right:
+            st.title("Sidebar")
+
+    def run(self):
+        self.create_rows()
+
+
+app = UI()
+app.run()
