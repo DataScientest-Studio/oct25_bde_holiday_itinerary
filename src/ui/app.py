4f54befe918903936dba1893b68961dfc8b385e9
Max Duessler <max.duessler@posteo.net>
2025-12-19 01:29:30 +0100

    Changed names.

--- src/ui/app.py
+++ src/ui/app.py
@@ -18,7 +18,7 @@ class UI:
         if "filter-type" not in st.session_state:
             st.session_state.filter_type = []
         if "select-pois" not in st.session_state:
-            st.session_state.select_pois = self.select_pois()
+            st.session_state.selected_pois = self.select_pois()
         if "add-pois" not in st.session_state:
             st.session_state.add_pois = ""
 
@@ -50,7 +50,7 @@ class UI:
         poi_filters, date_filters = cell.columns([1, 1])
         poi_filters.multiselect("Place / City to visit", options=self.select_locations(), key="filter-location")
         poi_filters.multiselect("Type of Place / City", options=self.select_types(), key="filter-type")
-        poi_filters.multiselect("POIs", options=self.select_pois, key="select-pois")
+        poi_filters.multiselect("POIs", options=st.session_state.selected_pois, key="select-pois")
         poi_filters.button("Add POIs", on_click=self.add_pois, key="add-pois", args=[self])
         date_filters.date_input("Start", format="DD/MM/YYYY")
         date_filters.date_input("End", format="DD/MM/YYYY")
