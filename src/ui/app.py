8a74f73605a6cb2764ba6e7c1173cb5a7d6ecfc5
Max Duessler <max.duessler@posteo.net>
2025-12-20 03:17:13 +0100

    Added code to filter pois for city/destinations.

--- src/ui/app.py
+++ src/ui/app.py
@@ -3,7 +3,7 @@ from typing import Any
 
 from requests import get
 from requests.models import HTTPError
-from streamlit import set_page_config, title
+from streamlit import columns, container, session_state, set_page_config, title
 
 from logger import logger
 
@@ -30,7 +30,7 @@ class UI:
     title_name: str = "Holiday Itinerary"
     layout: str = "wide"
 
-    def __init__(self) -> None:
+    def __init__(self, pois: dict[str, Any]) -> None:
         logger.debug("Initializing UI for holiday itinerary...")
 
         set_page_config(page_title=self.title_name, layout=self.layout)
@@ -43,8 +43,16 @@ class UI:
 
         logger.success("Initialized UI.")
 
+    def __init_session_states(self) -> None:
+        logger.debug("Initializing session states...")
+        if "destinations" not in session_state:
+            session_state.destinations = []
+
+        logger.success("Initialized session_states.")
+
     def __init_layout(self) -> None:
         logger.debug("Initializing layout...")
+        self.__init_controls()
         self.__init_data_layout()
         logger.info("Initalized layout.")
 
@@ -56,12 +64,25 @@ class UI:
 
     def __init_controls(self) -> None:
         logger.debug("Initializing controls...")
+        with container as con:
+            logger.debug("Created container for controls.")
+            destinations, _, _, _, _ = con.columns([])
+            self.__init_destination_filter(destinations)
 
         logger.info("Initalized controls.")
+
+    def __init_destination_filter(self, cell: columns) -> None:
+        logger.debug("Initializing destination filter...")
+        try:
+            destinations = handle_get_request("/city/")["cities"]
+            cell.multiselect("Itinerary Destinations", options=destinations, key="destinations")
+            logger.info("Initalized destination filter.")
+        except Exception:
+            logger.error("Failed to get destinations form the server.")
 
     def run(self) -> None:
         logger.info("Starting UI.")
 
 
-app = UI()
+app = UI({})
 app.run()
