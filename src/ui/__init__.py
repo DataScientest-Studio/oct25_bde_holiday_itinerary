0cce11cd3a7c1ab0ebe6f2b249de98c7b0be3cd5
Max Duessler <max.duessler@posteo.net>
2025-12-22 07:49:05 +0100

    Added init.py

---
+++ src/ui/__init__.py
@@ -0,0 +1,6 @@
+from .grid import Grid
+from .handler import Handler, handle_get_request
+from .poi import Poi
+from .ui import UI
+
+__all__ = ["Grid", "Handler", "handle_get_request", "Poi", "UI"]
