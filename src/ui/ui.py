ccc33c6f0defc51507ee1ea345ba826f74a2d08c
Max Duessler <max.duessler@posteo.net>
2026-01-04 06:17:48 +0100

    Fixed layout.

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -95,8 +95,9 @@ class UI:
             with pois_overview:
                 self.__init_pois_overview_layout()
         with poi_view:
-            self.__init_poi_overview_layout()
-            self.__init_poi_add_button()
+            with st.container(border=False, height=500):
+                self.__init_poi_overview_layout()
+                self.__init_poi_add_button()
         logger.info("Initialized overview sections.")
 
         map_grid, route = st.columns([8, 2], border=True)
@@ -197,23 +198,24 @@ class UI:
         )
 
     def __init_poi_overview_layout(self) -> None:
-        logger.debug("Initializing pois overview...")
-        if st.session_state.selected_poi is None:
-            st.subheader("POI Overview")
-            return
+        with st.container(border=False, height=435):
+            logger.debug("Initializing pois overview...")
+            if st.session_state.selected_poi is None:
+                st.subheader("POI Overview")
+                return
 
-        poi = st.session_state.selected_poi
+            poi = st.session_state.selected_poi
 
-        st.subheader(poi["label"])
-        st.caption(f"📍 {poi['street']}, {poi['postal_code']} {poi['city']}")
-        st.markdown("🌳 **Description**")
-        st.write(poi["description"] or "Point of interest has no description.")
-        if poi.get("additional_information"):
-            st.markdown("ℹ️ **Additional Information**")
-            st.write(poi["additional_information"])
-        if poi.get("homepage"):
-            st.markdown(f"🌐 **Website**: [🌐 Visit website]({poi['homepage']})")
-        logger.info("Initalized pois overview.")
+            st.subheader(poi["label"])
+            st.caption(f"📍 {poi['street']}, {poi['postal_code']} {poi['city']}")
+            st.markdown("🌳 **Description**")
+            st.write(poi["description"] or "Point of interest has no description.")
+            if poi.get("additional_information"):
+                st.markdown("ℹ️ **Additional Information**")
+                st.write(poi["additional_information"])
+            if poi.get("homepage"):
+                st.markdown(f"🌐 **Website**: [🌐 Visit website]({poi['homepage']})")
+            logger.info("Initalized pois overview.")
 
     def __init_map(self) -> None:
         logger.debug("Initializing map...")
