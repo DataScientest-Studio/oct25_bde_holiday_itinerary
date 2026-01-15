777ee736acb6670432eb057bb3c5b678f2e68e61
Max Duessler <max.duessler@posteo.net>
2026-01-15 13:13:37 +0100

    Finished itnierary calculator.

--- src/ui/handler.py
+++ src/ui/handler.py
@@ -75,12 +75,11 @@ class Handler:
             case "Round trip":
                 return self.roundtrip(pois, start)
             case "One-way trip (flexible end)":
-                pass
+                return self.one_way_trip_flex_end(pois, start)  # type: ignore[arg-type]
             case "One-way trip (fixed destination)":
-                pass
+                return self.one_way_trip_flex_fixed_end(pois, start, end)  # type: ignore[arg-type]
             case _:
                 raise ValueError(f"'{itinerary_type}' is not a valid itinerary_type.")
-        return pois, 0.0
 
     def roundtrip(self, pois: pd.DataFrame, start: str | None = None) -> tuple[pd.DataFrame, float]:
         params = self.prepare_params(pois, start)
@@ -92,14 +91,34 @@ class Handler:
         )
         return ordered_df, itinerary["total_distance"]
 
-    def prepare_params(self, pois: pd.DataFrame, poi_id: str | None = None) -> dict[str, Any]:
-        if poi_id:
-            poi_id = pois.loc[pois["label"] == poi_id, "poiId"].iloc[0]
+    def one_way_trip_flex_end(self, pois: pd.DataFrame, start: str) -> tuple[pd.DataFrame, float]:
+        params = self.prepare_params(pois, start)
+        itinerary = handle_get_request("/tsp/shortest-path-no-return", params)
+        ordered_df = pois.set_index("poiId").loc[itinerary["poi_order"]].reset_index()
+        return ordered_df, itinerary["total_distance"]
+
+    def one_way_trip_flex_fixed_end(self, pois: pd.DataFrame, start: str, end: str) -> tuple[pd.DataFrame, float]:
+        params = self.prepare_params(pois, start)
+        itinerary = handle_get_request("/tsp/shortest-path-no-return", params)
+        ordered_df = pois.set_index("poiId").loc[itinerary["poi_order"]].reset_index()
+        return ordered_df, itinerary["total_distance"]
+
+    def prepare_params(
+        self, pois: pd.DataFrame, poi_start: str | None = None, poi_end: str | None = None
+    ) -> dict[str, Any]:
+        if poi_start:
+            poi_start = pois.loc[pois["label"] == poi_start, "poiId"].iloc[0]
+        if poi_end:
+            poi_end = pois.loc[pois["label"] == poi_end, "poiId"].iloc[0]
         poi_ids = pois["poiId"].tolist()
-        if poi_id:
-            logger.debug(f"Start/End POI {poi_id} is set. Removing it from existing list.")
-            poi_ids.remove(poi_id)
-            poi_ids = [poi_id] + poi_ids
+        if poi_start:
+            logger.debug(f"Start POI {poi_start} is set. Removing it from existing list.")
+            poi_ids.remove(poi_start)
+            poi_ids = [poi_start] + poi_ids
+        if poi_end:
+            logger.debug(f"End POI {poi_end} is set. Removing it from existing list.")
+            pois.remove(poi_end)
+            poi_ids += [poi_end]
         logger.info("Created parameter for trip tour.")
         return {"poi_ids": poi_ids}
 
