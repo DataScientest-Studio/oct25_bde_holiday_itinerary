91ef122dca6ebdc11f5104fbaedb2a27d79c8e32
Max Duessler <max.duessler@posteo.net>
2026-01-16 10:58:59 +0100

    Fixed missing backslash

--- init/import_script.sh
+++ init/import_script.sh
@@ -15,7 +15,7 @@ if [ ! -d "$DB_DIR" ] || [ ! -f "$FLAG" ]; then
         --verbose \
         --multiline-fields=true \
         --nodes="City=/import/cities_nodes.zip" \
-        --relationships="ROAD_TO=/import/roads_rels.zip"
+        --relationships="ROAD_TO=/import/roads_rels.zip" \
         --nodes="POI=/import/poi_nodes.zip" \
         --nodes="POIType=/import/type_nodes.zip" \
         --relationships="IS_A=/import/poi_is_a_type_rels.zip"\
