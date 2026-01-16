509865c64f495e73b67bc4f4abc98037bb4f482a
Max Duessler <max.duessler@posteo.net>
2026-01-16 01:49:59 +0100

    Refactored map.

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,4 +1,5 @@
 from .handler import Handler, handle_get_request
+from .map import Map
 from .ui import UI
 
-__all__ = ["Handler", "handle_get_request", "UI"]
+__all__ = ["Handler", "handle_get_request", "Map", "UI"]
