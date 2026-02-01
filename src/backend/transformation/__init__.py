fa0f900a50cbe8db39cb513baf8e20327456efe1
Max Duessler <max.duessler@posteo.net>
2026-02-01 11:23:24 +0100

    Created option to create csv files for french cities.

--- src/backend/transformation/__init__.py
+++ src/backend/transformation/__init__.py
@@ -7,11 +7,13 @@ from .datatourisme import (
     process_data,
     store_nodes_and_edges,
 )
+from .french_cities import create_city_nodes
 
 __all__ = [
     "create_poi_is_a_type_rels_df",
     "create_poi_nodes_df",
     "create_type_nodes_df",
+    "create_city_nodes",
     "get_data_from_poi",
     "get_id_from_filename",
     "process_data",
--- src/backend/transformation/french_cities.py
+++ src/backend/transformation/french_cities.py
@@ -1,4 +1,3 @@
-import zipfile
 from pathlib import Path
 
 import pandas as pd
@@ -7,10 +6,9 @@ SRC_URL = "https://simplemaps.com/static/data/country-cities/fr/fr.csv"
 ROOT = Path(__file__).parent.parent.parent.parent
 OUTPUT_DIR = ROOT / "import_data"
 CSV_FILE = OUTPUT_DIR / "cities_nodes.csv"
-ZIP_FILE = OUTPUT_DIR / "cities_nodes.zip"
 
 
-def create_city_nodes(src: Path, dst: Path) -> None:
+def create_city_nodes() -> None:
     df = pd.read_csv(SRC_URL)
 
     city_nodes = pd.DataFrame(
@@ -27,9 +25,3 @@ def create_city_nodes(src: Path, dst: Path) -> None:
     )
 
     city_nodes.to_csv(CSV_FILE, index=False, quoting=1)  # csv.QUOTE_ALL
-
-    with zipfile.ZipFile(ZIP_FILE, "w", zipfile.ZIP_DEFLATED) as zipf:
-        zipf.write(CSV_FILE, arcname=CSV_FILE.name)
-
-    # 5. Delete the original CSV
-    CSV_FILE.unlink()
