fdfcc5951143fa0e641423b22481c21170a8ec68
Max Duessler <max.duessler@posteo.net>
2026-02-02 23:07:52 +0100

    Set url via env.

--- src/frontend/ui/handlers/get_request.py
+++ src/frontend/ui/handlers/get_request.py
@@ -1,15 +1,23 @@
+import sys
 from json import loads
+from os import getenv
 from typing import Any
 
 from loguru import logger
 from requests import get
 from requests.models import HTTPError
 
+URL = getenv("API_URL")
+
+if not URL:
+    logger.error("Need the env variable 'API_URL'. Exit now.")
+    sys.exit(1)
+
 
 def get_request(target: str, query_params: dict[str, str] | None = None) -> dict[str, Any]:
     logger.info(f"Sending GET request to http://neo4j_api:8080{target} with params: {query_params}")
     try:
-        response = get(f"http://neo4j_api:8080{target}", params=query_params)
+        response = get(f"{URL}{target}", params=query_params)
         logger.debug(f"Received response returned status code {response.status_code}")
 
         match response.status_code:
